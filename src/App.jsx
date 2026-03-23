d18ed82a009b1b3b012bc4aa99808774f6baf6eb
DashginAsgarli <dashqinasgarli@gmail.com>
2026-03-23 22:39:49 +0400

    feat: establish professional project directory structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <main>
+        
+      </main>
 
     </>
   )
---
+++ src/pages/MusicPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MusicPage() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default MusicPage
